bf1bbee154163989334077e3564562d77360e5e7
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-06 20:28:20 +0530

    final styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import data from "./data";
 import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <h1>Reminder</h1>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </main>
   );
 }
 
